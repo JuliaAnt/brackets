49cdd371ba49019f576be78b4be3c95c3341cc2e
Julia Antoshkina <julia.antoshkina@gmail.com>
2022-03-11 16:29:16 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const obj = {};
+  for (let a = 0; a < bracketsConfig.length; a++) {
+    let subArr = bracketsConfig[a];
+    for (let b = 0; b < subArr.length; b++) {
+      obj[subArr[0]] = subArr[1];
+    }
+  }
+
+  let arr = str.split("");
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let token = arr[i];
+
+    if (obj[token] === token) {
+      if (!sameToken(token, newArr)) {
+        return false;
+      }
+    } else {
+      if (obj[token]) {
+        newArr.push(token);
+      } else {
+        const last = newArr[newArr.length - 1];
+        if (obj[last] === token) {
+          newArr.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+
+  if (newArr.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function sameToken(token, newArr) {
+  let index = newArr.indexOf(token);
+  if (index > -1) {
+    if (index !== newArr.length - 1) {
+      return false;
+    }
+    newArr.pop();
+  } else {
+    newArr.push(token);
+  }
+  return true;
 }
